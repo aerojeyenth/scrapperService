325da5966c6f72b885713d961f9adda14fc3e71d
e066532 <Jeyenthaaran.Nanjappan@mastercard.com>
2017-06-30 05:19:31 +0530

    Comments on is loginpage service

--- services/isloginpage.js
+++ services/isloginpage.js
@@ -5,12 +5,15 @@ var isloginpage = function($){
     $('form').each(function (i, ele) {
 
         var frm = $(this);
+
+        //Check for a form with only 2 serialized array values
         if(frm.serializeArray().length === 2){
 
+            //Check if one of it is of type password.
             frm.find('input').each(function(i, ele){
                 if($(this).attr('type') === 'password'){
                     loginpage = true;
-                    
+
                     //Return false to break the .each of cheerio.js
                     return false;
                 }
